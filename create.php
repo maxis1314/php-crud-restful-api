84a6685f8a44d2973091d801116596653b721d69
daniel <maxis1314@gmail.com>
2019-03-10 14:49:28 +0800

    2019/03/10

--- create.php
+++ create.php
@@ -10,9 +10,10 @@ $uuid=$input['uuid'];
 $title=$input['title'];
 $content=$input['content'];
 $done=$input['done'];
+$deviceid=$input['deviceid'];
 
-$query = "replace into memo (uuid,title, content,done) values ('%s','%s','%s','%s')";
-$ret =  exec_sql($query,array($uuid,$title,$content,$done));
+$query = "replace into memo (uuid,title, content,done,deviceid) values ('%s','%s','%s','%s')";
+$ret =  exec_sql($query,array($uuid,$title,$content,$done,$deviceid));
  
 
 echo json_encode(
