f0886bff8b387a2a8613bb6545d76b97bc54ede2
daniel <maxis1314@gmail.com>
2019-03-10 19:09:30 +0800

    2019/03/10

---
+++ delete_one.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json');
+
+require("lib/utils.php");
+ 
+$json = file_get_contents('php://input');
+$input = json_decode($json,true);
+
+
+$deviceid=$input['deviceid'];
+$uuid=$input['uuid']; 
+
+
+$query = "delete from memo where $uuid='%s' and deviceid = '%s'";
+$ret =  exec_sql($query,array($uuid,$deviceid));
+ 
+
+echo json_encode(
+    array(
+        'code' => 0,
+        'ret'  => $ret?1:0
+    )
+);
+?>
